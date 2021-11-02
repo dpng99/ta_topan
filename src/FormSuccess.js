ec9d06ecd169003e772c7104b47d9d3eb8fd39b2
antaufany <antaufanypuji.r@gmail.com>
2021-11-03 01:47:41 +0700

    FormLogin2

--- src/FormSuccess.js
+++ src/FormSuccess.js
@@ -1,11 +1,51 @@
 import React from 'react';
+import validate from './ValidateLogin';
+import useForm from './UseFormLogin';
 import './Form.css';
 
-const FormSuccess = () => {
+const FormSuccess = ({ submitForm }) => {
+  const { handleChange, handleSubmit, values, errors } = useForm(
+    submitForm,
+    validate
+  );
   return (
     <div className='form-content-right'>
-      <h1 className='form-success'>We have received your request!</h1>
-      <img className='form-img-2' src='img/img-3.svg' alt='success-image' />
+      <form onSubmit={handleSubmit} className='form' noValidate>
+        <h1>
+          Selamat Datang! Dashboard Website PDAM Madiun 
+        </h1>
+        
+        <div className='form-inputs'>
+          <label className='form-label'>Email</label>
+          <input
+            className='form-input'
+            type='email'
+            name='email'
+            placeholder='Masukan Email'
+            value={values.email}
+            onChange={handleChange}
+          />
+          {errors.email && <p>{errors.email}</p>}
+        </div>
+        <div className='form-inputs'>
+          <label className='form-label'>Password</label>
+          <input
+            className='form-input'
+            type='password'
+            name='password'
+            placeholder='Masukan Password'
+            value={values.password}
+            onChange={handleChange}
+          />
+          {errors.password && <p>{errors.password}</p>}
+        </div>
+        
+        <button className='form-input-btn' type='submit'>
+          Sign in
+        </button>
+        <span className='form-input-login'>
+        </span>
+      </form>
     </div>
   );
 };
