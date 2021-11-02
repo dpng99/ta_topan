14188fe3791acbfe31262ebcdec6997f60519a00
Muhammad Daffa Ilyasa <muhammaddaffailyasa1@gmail.com>
2021-11-03 00:23:20 +0700

    Pertama

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Form from './Form';
+
+
+function App() {
+  <img className='form-img' src='img/mataair.jpeg' alt='spaceship' />
+  return <Form />;
+  
+}
+
+export default App;
---
+++ src/FormLogin.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import validate from './validateInfo';
+import useForm from './useForm';
+import './Form.css';
+
+const FormLogin = ({ submitForm }) => {
+  const { handleChange, handleSubmit, values, errors } = useForm(
+    submitForm,
+    validate
+  );
+
+  return (
+    <div className='form-content-right'>
+      <form onSubmit={handleSubmit} className='form' noValidate>
+        <h1>
+          Selamat Datang! Dashboard Website PDAM Madiun 
+        </h1>
+        
+        <div className='form-inputs'>
+          <label className='form-label'>Email</label>
+          <input
+            className='form-input'
+            type='email'
+            name='email'
+            placeholder='Masukan Email'
+            value={values.email}
+            onChange={handleChange}
+          />
+          {errors.email && <p>{errors.email}</p>}
+        </div>
+        <div className='form-inputs'>
+          <label className='form-label'>Password</label>
+          <input
+            className='form-input'
+            type='password'
+            name='password'
+            placeholder='Masukan Password'
+            value={values.password}
+            onChange={handleChange}
+          />
+          {errors.password && <p>{errors.password}</p>}
+        </div>
+        
+        <button className='form-input-btn' type='submit'>
+          Sign in
+        </button>
+        <span className='form-input-login'>
+        </span>
+      </form>
+    </div>
+  );
+};
+
+export default FormLogin;
