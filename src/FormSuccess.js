6e3cd63d669481ae3c5cd68cdcf0536b04dc782c
Muhammad Daffa Ilyasa <muhammaddaffailyasa1@gmail.com>
2021-11-03 11:22:36 +0700

    click astronaut to login page

--- src/FormSuccess.js
+++ src/FormSuccess.js
@@ -1,51 +1,14 @@
 import React from 'react';
-import validate from './ValidateLogin';
-import useForm from './UseFormLogin';
+import { BrowserRouter as Redirect } from 'react-router-dom';
 import './Form.css';
 
-const FormSuccess = ({ submitForm }) => {
-  const { handleChange, handleSubmit, values, errors } = useForm(
-    submitForm,
-    validate
-  );
+const FormSuccess = () => {
   return (
     <div className='form-content-right'>
-      <form onSubmit={handleSubmit} className='form' noValidate>
-        <h1>
-          Selamat Datang! Dashboard Website PDAM Madiun 
-        </h1>
-        
-        <div className='form-inputs'>
-          <label className='form-label'>Email</label>
-          <input
-            className='form-input'
-            type='email'
-            name='email'
-            placeholder='Masukan Email'
-            value={values.email}
-            onChange={handleChange}
-          />
-          {errors.email && <p>{errors.email}</p>}
-        </div>
-        <div className='form-inputs'>
-          <label className='form-label'>Password</label>
-          <input
-            className='form-input'
-            type='password'
-            name='password'
-            placeholder='Masukan Password'
-            value={values.password}
-            onChange={handleChange}
-          />
-          {errors.password && <p>{errors.password}</p>}
-        </div>
-        
-        <button className='form-input-btn' type='submit'>
-          Sign in
-        </button>
-        <span className='form-input-login'>
-        </span>
-      </form>
+      <h1 className='form-success'>We have received your request!</h1>
+        <a href='/login'>
+      <img className='form-img-2' src='img/img-3.svg' alt='success-image' />
+      </a>
     </div>
   );
 };
