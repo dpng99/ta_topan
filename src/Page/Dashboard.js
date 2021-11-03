1ade2855471a45ec6d5f069162a18dbc10b08683
antaufany <antaufanypuji.r@gmail.com>
2021-11-04 01:51:09 +0700

    GIS2

--- src/Page/Dashboard.js
+++ src/Page/Dashboard.js
@@ -1,23 +1,29 @@
 import React from 'react'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-
+import { MapContainer, TileLayer, Marker, Popup } 
+from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
+import './Dashboard.css'
 const Dashboard = () => {
-    return (
-        <div tyle="background-color: #ffff00; border-width:1px; border-style: solid; border-color: #000000; height: 500px">
-           
+      
+
+        return (
+            <div className= "leaflet-container">
 <MapContainer center={[111.4930317, -7.6300605]} zoom={13} scrollWheelZoom={false}>
-  <TileLayer
-    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  <Marker position={[111.4930317, -7.6300605]}>
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-  </Marker>
-</MapContainer> 
-        </div>
-    )
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[111.4930317, -7.6300605]}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
+            </div>
+            
+
+        );
 }
+    
 
 export default Dashboard
