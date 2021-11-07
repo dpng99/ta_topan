75ac4a0e6051162844a288c56c8a88e55b457a80
antaufany <antaufanypuji.r@gmail.com>
2021-11-08 00:00:10 +0700

    TAMPILAN_DASHBOARD

--- src/Page/Dashboard.js
+++ src/Page/Dashboard.js
@@ -2,11 +2,41 @@ import React from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
 import './Dashboard.css'
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
+
 const Dashboard = () => {
       
 
         return (
-            <div className= "leaflet-container ">
+           <>
+ <Navbar bg="primary" expand="lg">
+  <Container>
+  <Navbar.Brand href="#home">
+        <img
+          alt=""
+          src="/pdam1.jpg"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{' '}
+      React Bootstrap
+      </Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <NavDropdown title="Menu" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Edit</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+
+ <div className= "leaflet-container ">
               <div className="leaflet-wrapper">
 <           MapContainer center={[-7.6300605, 111.4930317]} zoom={13} scrollWheelZoom={false}>
             <TileLayer
@@ -21,6 +51,7 @@ const Dashboard = () => {
           </MapContainer>
           </div>
             </div>
+            </>
             
 
         );
