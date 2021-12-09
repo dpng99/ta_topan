ece06d34c93ea07a44bc4ba26c649f694012c052
Dpng99 <muhammaddaffailyasa1@gmail.com>
2021-12-10 01:46:44 +0700

    bisa update ajg

--- src/Page/Monitoring.js
+++ src/Page/Monitoring.js
@@ -1,12 +1,35 @@
 import React,{useState, useEffect} from 'react'
-import { Container, Dropdown, Image} from 'react-bootstrap'
+import { Container, Dropdown, Image, ListGroup, Button, Card} from 'react-bootstrap'
 import Navbarx from '../Component/Navbar'
 import CRUDHandler from '../Handler/CRUDHandler'
 
 const Monitoring = () => {
+    
     const [DataSet, setDataSet] = useState('')
     const [ DataChild, setDataChild] = useState('')
     const [ DataKey, setDataKey] = useState('')
+    const [ currentData, setcurrentData] = useState('')
+    const [currentIndex, setCurrentIndex] = useState([])
+
+    const settDataEdit = (index) => {
+    
+        const dataBind = CRUDHandler.getMonitor()
+        dataBind.on('value', (snapshot) => {
+            const DataBinning = snapshot.child('LokasiDebit').child(index).child('Submitted').val()
+            for(let id in DataBinning) {
+                    currentIndex.push(DataBinning[id])      
+            }
+                     
+            
+        })
+        
+        
+        setCurrentIndex(currentIndex)
+        console.log(currentIndex)   
+    }
+ 
+     
+  
     useEffect(() => {
         const enumerate = CRUDHandler.getMonitor()
         enumerate.on('value', (snapshot) => {
@@ -20,29 +43,39 @@ const Monitoring = () => {
             for(let id in dataSnip2) {
                 DataKey.push(dataSnip2[id])
             }
-
+        
             setDataSet(DataSet)
             setDataKey(DataKey)
+            
+            
         })
+        
+      
+     
+
     }, [])
     
     return (
         <>
         <Navbarx/>
-        <Image src='/img/bg2.jpg' style={{ width: '100%', height: '100%', overflow: 'hidden', position: 'absolute'}}/>
-        <Container>
-            <Dropdown fluid className='align-content-left justify-content-left d-grid grid-content' style={{ margin: '10px 50px 50px 50px', padding: '50px  50px 50px 50px' }}>
+        
+        <Container className='align-content-right justify-content-right d-flex flex-center'>
+            <Dropdown fluid >
             <Dropdown.Toggle  variant="success" id="dropdown-basic">
              Lokasi Debit
              </Dropdown.Toggle>
 
-         <Dropdown.Menu >{DataSet ? DataSet.map((item, index) => (
-             <Dropdown.Item key={index}>{item.nama}</Dropdown.Item>
-         )): null}
+         <Dropdown.Menu >
+             {Object.keys(DataSet).map((item, index) => (
+             <Dropdown.Item key={item} onClick={() => settDataEdit(DataSet[item].nama)
+            }>
+                {DataSet[item].nama}
+                </Dropdown.Item>
+         ))}
         </Dropdown.Menu>
         </Dropdown>
 
-        <Dropdown fluid className='align-content-right justify-content-right d-grid grid-content' style={{ margin: '50px 50px 50px 50px', padding: '50px  50px 50px 50px' }}>
+        <Dropdown fluid  >
             <Dropdown.Toggle variant="success" id="dropdown-basic">
              Lokasi Quality
              </Dropdown.Toggle>
@@ -54,8 +87,28 @@ const Monitoring = () => {
         
         </Dropdown.Menu>
         </Dropdown>
-
+        <Button className="btn btn-primary" style={{ position: 'relative' }} onClick={() => window.location.reload(false)}> Tombol</Button>
         </Container>
+        <Container>
+                <Card>
+                           {currentIndex && currentIndex.map((item, i) => (
+                             <Card fluid key={i} >
+                             <Card.Body>
+                                 <Card.Title>
+                                 {item.pipa}
+                                 </Card.Title>
+                                 <Card.Text>
+                                     {item.date}
+                                     {item.time}
+                                     {item.flowrate}
+                                 </Card.Text>
+                             </Card.Body>
+                         </Card>
+                               
+                           ))}
+                </Card>
+        </Container>
+        
         </>
     )
 }
