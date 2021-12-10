713fd4a92aa3d7ed2e0bd50814062d222ecadc66
Dpng99 <muhammaddaffailyasa1@gmail.com>
2021-12-11 02:54:11 +0700

    commit gajadi

--- src/Page/Edit.js
+++ src/Page/Edit.js
@@ -1,4 +1,4 @@
-import { Container, ListGroup, Card } from 'react-bootstrap'
+import { Container, ListGroup, Card, Button } from 'react-bootstrap'
 import Navbarx from '../Component/Navbar'
 import CRUDHandler from '../Handler/CRUDHandler'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,40 +11,60 @@ export class Edit extends Component {
     this.refreshlist = this.refreshlist.bind(this)
     this.setDataEdit = this.setDataEdit.bind(this)
     this.onDataChange = this.onDataChange.bind(this)
+    this.onDataChange2 = this.onDataChange2.bind(this)
+    this.setAlat = this.setAlat.bind(this)
     this.state = {
     dataSet: [],
+    dataSet2: [], 
     currentData: null,
     currentIndex: -1,
+    dataDebit: null,
+    dataQuality: null
     };
 }
 componentDidMount(){
-CRUDHandler.getLocation().on('value', this.onDataChange)
+CRUDHandler.getLocation().child('LokasiDebit').on('value', this.onDataChange) 
+CRUDHandler.getLocation().child('LokasiQuality').on('value', this.onDataChange2)
+CRUDHandler.getLocation().on('value', this.setAlat)
 
 }
 componentWillUnmount(){
-   CRUDHandler.getLoca().off('value', this.onDataChange)
+   CRUDHandler.getLocation().off('value', this.onDataChange)
+   CRUDHandler.getLocation().off('value', this.onDataChange2)
 }
+
 onDataChange(items){
    let dataset =[];
    items.forEach((item) =>{
        let key = item.key
-        let data = item.child('/Submitted').val();
+        let data = item.val();
         dataset.push({
             key: key,
-            date: data.date,
-            orp: data.orp,
-            ph: data.ph,
-            nama: data.sumur,
-            tds: data.tds,
-            temperature: data.temperature,
-            waktu: data.time,
-            turbidity: data.turbidity
+             nama: data.nama,
+             latitude: data.latitude,
+             longitude: data.longitude,
 
          
         })
     })
     this.setState({dataset: dataset})
+    console.log(dataset)
 }
+onDataChange2(items){
+    let dataset =[];
+    items.forEach((item) =>{
+        let key = item.key
+         let data = item.val();
+         dataset.push({
+             key: key,
+             nama: data.nama,
+             latitude: data.latitude,
+             longitude: data.longitude,
+          
+         })
+     })
+     this.setState({dataset: dataset})
+ }
 refreshlist(){
    this.setState({
         currentData: null,
@@ -57,12 +77,31 @@ setDataEdit(datum, index){
        currentIndex: index,
    });
 }
+setAlat(item){
+     let dataSet2 = []
+     item.forEach((item) =>{
+         let key = item.key
+         let data = item.val()
+        
+     })
+     this.setState({dataSet2: dataSet2})
+     console.log(dataSet2)
+}
+
+ 
 
     render() {
-        const {dataset, currentData, currentIndex} = this.state
+        const {dataset,dataSet2, currentData, currentIndex, dataDebit, dataQuality} = this.state
         return (
            <Container fluid="xxl">
                <Navbarx/>
+               {dataSet2.map((item, key) => 
+               <>
+               <Button key={key} onClick={() => this.setAlat(dataDebit)}>{item.nama}</Button>
+               </>
+             
+               )}
+               
                <Card>
                    <Card.Body>
                        <ListGroup as="ul">
